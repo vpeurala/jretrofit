bd6694ed0ba7ae7022eb1e085662204438a5b8f7
Ville Peurala <ville.peurala@gmail.com>
2006-11-18 18:43:17 +0000

    Implemented "complete" retrofitting.

--- org/laughingpanda/jretrofit/Retrofitter.java
+++ org/laughingpanda/jretrofit/Retrofitter.java
@@ -4,10 +4,12 @@ package org.laughingpanda.jretrofit;
  * @author Ville Peurala
  */
 public interface Retrofitter {
-    public Retrofitter withMethodLookupCaching();
-    public Retrofitter withoutMethodLookupCaching();
+    public Object complete(Object target, Class interfaceToImplement)
+            throws AllMethodsNotImplementedException;
+    public Object complete(Object target, Class[] interfacesToImplement)
+            throws AllMethodsNotImplementedException;
     public Object partial(Object target, Class interfaceToImplement);
     public Object partial(Object target, Class[] interfacesToImplement);
-    //public Object complete(Object target, Class interfaceToImplement);
-    //public Object complete(Object target, Class[] interfacesToImplement);
+    public Retrofitter withMethodLookupCaching();
+    public Retrofitter withoutMethodLookupCaching();
 }
--- org/laughingpanda/jretrofit/RetrofitterWithoutMethodLookupCaching.java
+++ org/laughingpanda/jretrofit/RetrofitterWithoutMethodLookupCaching.java
@@ -1,39 +1,18 @@
 package org.laughingpanda.jretrofit;
 
-import java.lang.reflect.Proxy;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 /**
  * @author Ville Peurala
  */
-class RetrofitterWithoutMethodLookupCaching implements Retrofitter {
-    public Object partial(Object target, Class interfaceToImplement) {
-        return partial(target, new Class[] { interfaceToImplement });
+class RetrofitterWithoutMethodLookupCaching extends AbstractRetrofitter {
+    protected AbstractMethodLookupHelper createMethodLookupHelper(Object target) {
+        return new NonCachingMethodLookupHelper(target);
     }
 
-    public Object partial(Object target, Class[] interfacesToImplement) {
-        return Proxy.newProxyInstance(target.getClass().getClassLoader(),
-                allInterfacesToImplement(target, interfacesToImplement),
-                new RetrofitInvocationHandler(new NonCachingMethodLookupHelper(
-                        target)));
-    }
-
-    public Retrofitter withMethodLookupCaching() {
+    public final Retrofitter withMethodLookupCaching() {
         return new RetrofitterWithMethodLookupCaching();
     }
 
-    public Retrofitter withoutMethodLookupCaching() {
+    public final Retrofitter withoutMethodLookupCaching() {
         return this;
-    }
-
-    private static Class[] allInterfacesToImplement(Object target,
-            Class[] interfacesToImplement) {
-        ArrayList allInterfacesToImplement = new ArrayList();
-        allInterfacesToImplement.addAll(Arrays.asList(interfacesToImplement));
-        allInterfacesToImplement.addAll(Arrays.asList(target.getClass()
-                .getInterfaces()));
-        return (Class[]) allInterfacesToImplement
-                .toArray(new Class[allInterfacesToImplement.size()]);
     }
 }
