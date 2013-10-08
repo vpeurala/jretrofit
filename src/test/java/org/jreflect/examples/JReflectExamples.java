b9e045a8693f5b3ac4051f7346950b18acdadcd8
Ville Peurala <ville.peurala@gmail.com>
2013-10-08 11:21:34 +0300

    One more example.

--- src/test/java/org/jreflect/examples/JReflectExamples.java
+++ src/test/java/org/jreflect/examples/JReflectExamples.java
@@ -1,6 +1,7 @@
 package org.jreflect.examples;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 import java.util.Random;
 
@@ -10,21 +11,37 @@ import org.junit.Test;
 public class JReflectExamples {
     private static class ExampleClass {
         public int argumentOfMethodWithPrimitiveIntParameter;
+        public Integer argumentOfMethodWithObjectIntegerParameter;
 
         public void methodWithPrimitiveIntParameter(int input) {
             argumentOfMethodWithPrimitiveIntParameter = input;
+        }
+
+        public void methodWithObjectIntegerParameter(Integer input) {
+            argumentOfMethodWithObjectIntegerParameter = input;
         }
     }
 
     private final Random random = new Random();
     private final ExampleClass example = new ExampleClass();
 
+    @SuppressWarnings("boxing")
     @Test
-    public void testSetterCanBeInvoked() {
+    public void primitiveIntArgument() {
         final int randomInt = random.nextInt();
+
         Reflect.on(example).method("methodWithPrimitiveIntParameter")
-                .invoke(new Integer(randomInt));
+                .invoke(randomInt);
         assertEquals(randomInt,
                 example.argumentOfMethodWithPrimitiveIntParameter);
+    }
+
+    @Test
+    public void objectIntegerArgument() {
+        final Integer randomInteger = new Integer(random.nextInt());
+        Reflect.on(example).method("methodWithObjectIntegerParameter")
+                .invoke(randomInteger);
+        assertSame(randomInteger,
+                example.argumentOfMethodWithObjectIntegerParameter);
     }
 }
